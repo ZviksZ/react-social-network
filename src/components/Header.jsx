aacf20a49a826eb059d236b660a03338a825f92b
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2019-10-20 11:58:59 +0300

    start project

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                className="header_logo"
+                src="https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo-title-dark.png"
+                alt=""/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className="content">
+
+            <img className="main-img"
+                 src="https://coolbackgrounds.io/images/backgrounds/index/sea-edge-79ab30e2.png"
+                 alt=""/>
+
+            <div className="content_info">
+                <img src="https://mdn.mozillademos.org/files/16631/favicon57.de33179910ae.1.1.png" alt=""/>
+                <div className="content_user-data">
+
+                </div>
+            </div>
+            <div className="my-post"></div>
+            <div className="new-post"></div>
+            <div className="post"></div>
+            <div className="post"></div>
+        </div>
+    );
+}
+
+export default Profile;
