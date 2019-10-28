9b0a69adba01255dbce4ce6b834057c1e8d5560a
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2019-10-29 00:17:54 +0300

    Рефакторинг кода

--- src/render.js
+++ src/render.js
@@ -4,12 +4,14 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { addPost } from "./redux/state";
+import { updateNewPostText } from "./redux/state";
+import { newPostText } from "./redux/state";
 
 
 export let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={ state } addPost={ addPost }/>
+            <App state={ state } addPost={ addPost } updateNewPostText={updateNewPostText} />
         </BrowserRouter>, document.getElementById('root'));
 }
 
