4491f1a81b149544871d591de7a40942d3076c69
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2019-11-08 16:30:59 +0300

    Рефакторинг - Классовая компонента Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,89 +1,50 @@
-import React from 'react'
+import React, {Component} from 'react'
 import s     from './Users.module.css';
-
-let usersData = [
-    {
-        id: 1,
-        photoUrl: 'https://www.meme-arsenal.com/memes/7bdea6754f999b50e9577596f09197fb.jpg',
-        fullName: 'Dmitry',
-        status: 'I am a boss',
-        location: {
-            city: 'Minsk',
-            country: 'Belarus'
-        },
-        followed: false
-    },
-    {
-        id: 2,
-        photoUrl: 'https://www.meme-arsenal.com/memes/7bdea6754f999b50e9577596f09197fb.jpg',
-        fullName: 'Vasya',
-        status: 'I am a Vasya',
-        location: {
-            city: 'Moscow',
-            country: 'Russia'
-        },
-        followed: true
-    },
-    {
-        id: 3,
-        photoUrl: 'https://www.meme-arsenal.com/memes/7bdea6754f999b50e9577596f09197fb.jpg',
-        fullName: 'Roman',
-        status: 'I am a killer',
-        location: {
-            city: 'Kiev',
-            country: 'Ukraine'
-        },
-        followed: false
-    },
-    {
-        id: 4,
-        photoUrl: 'https://www.meme-arsenal.com/memes/7bdea6754f999b50e9577596f09197fb.jpg',
-        fullName: 'Gunter',
-        status: 'I am a don',
-        location: {
-            city: 'Berlin',
-            country: 'Germany'
-        },
-        followed: true
-    },
-]
+import * as axios from 'axios'
+import userPhoto from '../../assets/images/default_ava.jpg'
 
 
-const Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers(usersData);
+class Users extends React.Component {
+
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => this.props.setUsers(response.data.items))
     }
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id} className={s.userItem}>
-                    <div>
-                        <img src={u.photoUrl} className={s.avatar} alt=""/>
-                        {
-                            u.followed ?
-                                <button onClick={() => {
-                                    props.unfollow(u.id)
-                                }}>Unfollow</button>
-                                :
-                                <button onClick={() => {
-                                    props.follow(u.id)
-                                }}>Follow</button>
-                        }
-                    </div>
-                    <div className={s.wrap}>
-                        <div className={s.left}>
-                            <h3>{u.fullName}</h3>
-                            <p>{u.status}</p>
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id} className={s.userItem}>
+                        <div>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={s.avatar} alt=""/>
+                            {
+                                u.followed ?
+                                    <button onClick={() => {
+                                        this.props.unfollow(u.id)
+                                    }}>Unfollow</button>
+                                    :
+                                    <button onClick={() => {
+                                        this.props.follow(u.id)
+                                    }}>Follow</button>
+                            }
                         </div>
-                        <div className={s.right}>
-                            <div className="">{u.location.country}</div>
-                            <div className="">{u.location.city}</div>
+                        <div className={s.wrap}>
+                            <div className={s.left}>
+                                <h3>{u.name}</h3>
+                                <p>{u.status}</p>
+                            </div>
+                            <div className={s.right}>
+                                <div className="">{"u.location.country"}</div>
+                                <div className="">{"u.location.city"}</div>
+                            </div>
                         </div>
-                    </div>
-                </div>)
-            }
-        </div>
-    )
+                    </div>)
+                }
+            </div>
+        )
+    }
 }
+
 
 export default Users;
