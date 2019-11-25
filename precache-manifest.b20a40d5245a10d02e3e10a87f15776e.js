6067c45076bf231387c6dc50611dc64267759d8d
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2019-11-25 09:33:33 +0300

    Updates

---
+++ precache-manifest.b20a40d5245a10d02e3e10a87f15776e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bf6a9cbe7b2525b7160a32560e8196",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "cb375751b8f787a46c44",
+    "url": "/react-social-network/static/css/3.b4515f96.chunk.css"
+  },
+  {
+    "revision": "75b2fd0adb3a90be2228",
+    "url": "/react-social-network/static/css/4.10e6a994.chunk.css"
+  },
+  {
+    "revision": "7d49335dd565bd0fb506",
+    "url": "/react-social-network/static/css/5.a829585f.chunk.css"
+  },
+  {
+    "revision": "8d5bfafff818be4412bd",
+    "url": "/react-social-network/static/css/main.0945c028.chunk.css"
+  },
+  {
+    "revision": "3cbf690b0b04022c50e9",
+    "url": "/react-social-network/static/js/2.f1d2e986.chunk.js"
+  },
+  {
+    "revision": "cb375751b8f787a46c44",
+    "url": "/react-social-network/static/js/3.fc6f3dca.chunk.js"
+  },
+  {
+    "revision": "75b2fd0adb3a90be2228",
+    "url": "/react-social-network/static/js/4.e07ecb62.chunk.js"
+  },
+  {
+    "revision": "7d49335dd565bd0fb506",
+    "url": "/react-social-network/static/js/5.3d50f930.chunk.js"
+  },
+  {
+    "revision": "8d5bfafff818be4412bd",
+    "url": "/react-social-network/static/js/main.3131782a.chunk.js"
+  },
+  {
+    "revision": "6b9b02b0c18c438baeae",
+    "url": "/react-social-network/static/js/runtime-main.40c990bd.js"
+  },
+  {
+    "revision": "aca5023da590b65cb8eb315643c15b55",
+    "url": "/react-social-network/static/media/default_ava.aca5023d.jpg"
+  }
+]);
